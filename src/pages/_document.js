5c624a5ab901093b9b3a849eaeaf3953e71c9f8c
unknown <mikewhite628@gmail.com>
2023-03-22 16:26:30 -0500

    finished update of portfolio

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,13 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          src="https://kit.fontawesome.com/6f06d95fd6.js"
-          crossOrigin="anonymous"
-          async
-        ></script>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
